760a3f2b0b8741cfb91d828b0831c863ab92762d
Mattia Serafini <mattiaserafini0@gmail.com>
2025-02-17 16:09:26 +0100

    aggiunto il file.json con una fetch per la navbar

--- src/components/navbarTemporanea/NavbarTemporanea.jsx
+++ src/components/navbarTemporanea/NavbarTemporanea.jsx
@@ -1,13 +1,59 @@
 import { NavLink } from "react-router-dom";
 import { ViewModeContext } from "../../context/ViewModeContext";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 
 export default function NavbarTemporanea() {
   const { viewMode } = useContext(ViewModeContext);
   const inactiveColor = viewMode === "dark-mode" ? "white" : "black";
+
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const url = "/file.json";
+
+    fetch(url)
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Errore");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setData(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error.message);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error}</div>;
+
   return (
     <>
-      <nav>
+      <ul>
+        {data.map((i) => {
+          return (
+            <li key={i.id}>
+              <NavLink
+                to={`${i.link}`}
+                style={({ isActive }) => {
+                  return {
+                    color: isActive ? "red" : inactiveColor,
+                  };
+                }}
+              >
+                {i.category}
+              </NavLink>
+            </li>
+          );
+        })}
+      </ul>
+      {/* <nav>
         <NavLink
           to="firstsection"
           style={({ isActive }) => {
@@ -40,7 +86,7 @@ export default function NavbarTemporanea() {
         >
           Contatti
         </NavLink>
-      </nav>
+      </nav> */}
     </>
   );
 }
