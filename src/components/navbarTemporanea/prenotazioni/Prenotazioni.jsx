acb491bf0d819aef7ffc73026f5dad2d10db04a0
Mattia Serafini <mattiaserafini0@gmail.com>
2025-02-19 10:40:07 +0100

    applicato stile uniforme per i navlink

--- src/components/navbarTemporanea/prenotazioni/Prenotazioni.jsx
+++ src/components/navbarTemporanea/prenotazioni/Prenotazioni.jsx
@@ -1,13 +1,25 @@
+import { useContext } from "react";
 import { NavLink, Outlet } from "react-router-dom";
+import { ViewModeContext } from "../../../context/ViewModeContext";
 
 export default function Prenotazioni() {
   const prenotazioni = [1, 2, 3, 4, 5];
+  const { viewMode } = useContext(ViewModeContext);
+  const inactiveColor = viewMode === "dark-mode" ? "white" : "black";
   return (
     <>
       <h1>Prenotazioni</h1>
       {prenotazioni.map((i) => {
         return (
-          <NavLink key={i} to={`${i}`}>
+          <NavLink
+            key={i}
+            to={`${i}`}
+            style={({ isActive }) => {
+              return {
+                color: isActive ? "red" : inactiveColor,
+              };
+            }}
+          >
             Prenotazione {i}
           </NavLink>
         );
